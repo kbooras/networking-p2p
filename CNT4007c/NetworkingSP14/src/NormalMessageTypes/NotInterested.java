09a64615cf04c2d85dfcdb57702f5ef16a8b7f78
Kirstie <kbooras@ufl.edu>
2014-03-01 10:28:53 -0500

    "not interested' commit 3/1/14
    Removed ‘payload’ property as ‘not interested’ message types have no
    payload.
    Added functionality to add information to log file when a peer receives
    a ‘not interested’ message.

--- CNT4007c/NetworkingSP14/src/NormalMessageTypes/NotInterested.java
+++ CNT4007c/NetworkingSP14/src/NormalMessageTypes/NotInterested.java
@@ -1,4 +1,9 @@
 package NormalMessageTypes;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.util.Date;
 
 public class NotInterested {
 	int length;
@@ -7,7 +12,7 @@ public class NotInterested {
 	public NotInterested(int length, String payload)
 	{
 		this.length = length;
-		this.payload = payload;
+		//not interested has no payload
 	}
 
 	public int getLength()
@@ -30,6 +35,27 @@ public class NotInterested {
 	//Functionality
 	public void doSomething()
 	{
+		//generate log
+		try {
+			Date date = new Date();
+			String log = "[" + date.toString() + "]: Peer " + receivingID + 
+			" received a 'not interested' message from " + sendingID + " for the piece " + payload;
 
+			String fileName = "log_peer_[" + receivingID + "].log";
+			File file = new File(fileName);
+
+			//if file for log does not exist, make it
+			if (!file.exists()) {
+				file.createNewFile();
+			}
+
+			//append data to the file
+			FileWriter fileWriter = new FileWriter(file.getName(), true);
+			BufferedWriter bufferWriter = new BufferedWriter(fileWriter);
+			bufferWriter.write(log);
+			bufferWriter.close();
+		}catch(IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
