c3047bf9f7acce538451a13a7a0819bcd400a2d7
Kirstie <kbooras@ufl.edu>
2014-03-01 10:24:57 -0500

    'interested' commit 3/1/14
    Removed ‘payload’ property as ‘interested’ messages have no payload.
    Created functionality to add data to the log file.
    Once a peer receives an interested message, it should send that piece
    to the peer sending the message. Therefore, this method should notify
    the receiving peer to sendPiece(pieceIndex).  (This method can be
    better executed once functionality for sending pieces exists).

--- CNT4007c/NetworkingSP14/src/NormalMessageTypes/Interested.java
+++ CNT4007c/NetworkingSP14/src/NormalMessageTypes/Interested.java
@@ -7,7 +7,7 @@ public class Interested {
 	public Interested(int length, String payload)
 	{
 		this.length = length;
-		this.payload = payload;
+		//Interested has no payload	
 	}
 	
 	public int getLength()
@@ -18,18 +18,32 @@ public class Interested {
 	{
 		this.length = length;
 	}
-	public String getPayload()
-	{
-		return payload;
-	}
-	public void setPayload(String payload)
-	{
-		this.payload = payload;
-	}
 	
 	//Functionality
-	public void doSomething()
+	//**The peer should send the piece now. No method exists yet for this
+	public void doSomething(int sendingID, int receivingID)
 	{
+		//generate log
+		try {
+			Date date = new Date();
+			String log = "[" + date.toString() + "]: Peer " + receivingID + 
+			" received an 'interested' message from " + sendingID + " for the piece " + payload;
 
+			String fileName = "log_peer_[" + receivingID + "].log";
+			File file = new File(fileName);
+
+			//if file for log does not exist, make it
+			if (!file.exists()) {
+				file.createNewFile();
+			}
+
+			//append data to the file
+			FileWriter fileWriter = new FileWriter(file.getName(), true);
+			BufferedWriter bufferWriter = new BufferedWriter(fileWriter);
+			bufferWriter.write(log);
+			bufferWriter.close();
+		}catch(IOException e) {
+			e.printStackTrace();
+		}	
 	}
 }
