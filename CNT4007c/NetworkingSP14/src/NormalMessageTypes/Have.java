71b6492c5b40738ddbe865fde2ed5b49646b2389
Kirstie <kbooras@ufl.edu>
2014-03-01 10:15:16 -0500

    'have' commit 3/1/14
    Created functionality so that if peer A receives a ‘have’ message from
    peer B for a piece it does not have (using method ‘hasPiece() to check.
    Method does not exist yet, but should be in another class), A sends an
    ‘interested’ message to C. Else, A sends a ‘not interested’ message.
    This method also adds information to the log file.
    The method should also allow the receiving peer to update bitfields to
    all neighbors. This has not been implemented yet as there is not a
    class for bitfield yet.

--- CNT4007c/NetworkingSP14/src/NormalMessageTypes/Have.java
+++ CNT4007c/NetworkingSP14/src/NormalMessageTypes/Have.java
@@ -1,8 +1,14 @@
 package NormalMessageTypes;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.util.Date;
+
 
 public class Have {
 	int length;
-	String payload;
+	String payload; //4-byte index of file piece. which is byte[] payload = new byte[4];
 	
 	public Have(int length, String payload)
 	{
@@ -25,11 +31,52 @@ public class Have {
 	public void setPayload(String payload)
 	{
 		this.payload = payload;
+
 	}
 	
 	//Functionality
-	public void doSomething()
+	//what happens when you receive this message
+	public void doSomething(int sendingID, int receivingID)
 	{
+		//if peer A receives a have message from peer B that contains the index of a piece that peer A does not have, 
+		//then peer A sends an interested message to peer B
+		if (receivingID.hasPiece(payload)) {
+			NormalMessage newMessage = (length, 2, null); //find out the length, interested has no payload
+			receivingID.sendMessage(newMessage, sendingID);
+		}
+		//else, peer A sends a not interested message to B
+		else if (!receivingID.hasPiece(payload)) {
+			NormalMessage newMessage = (length, 3, null); //find out the length, not interested has no payload
+			receivingID.sendMessage(newMessage, sendingID);
+		}
 
+		//generate log
+		try {
+			Date date = new Date();
+			String log = "[" + date.toString() + "]: Peer " + receivingID + 
+			" received a 'have' message from " + sendingID + " for the piece " + payload;
+
+			String fileName = "log_peer_[" + receivingID + "].log";
+			File file = new File(fileName);
+
+			//if file for log does not exist, make it
+			if (!file.exists()) {
+				file.createNewFile();
+			}
+
+			//append data to the file
+			FileWriter fileWriter = new FileWriter(file.getName(), true);
+			BufferedWriter bufferWriter = new BufferedWriter(fileWriter);
+			bufferWriter.write(log);
+			bufferWriter.close();
+		}catch(IOException e) {
+			e.printStackTrace();
+		}
+		
+		//peers maintain bitfields to all neighbors and updates them whenever it receives a have message from a neighbor
+		//***Not sure how to do this***
+		
 	}
+
+
 }
