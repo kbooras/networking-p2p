4605dba9350a644485b606048d5146a9825f4e45
Kirstie <kbooras@ufl.edu>
2014-04-07 10:48:28 -0400

    bitfield 4/7

--- CNT4007c/NetworkingSP14/src/NormalMessageTypes/Bitfield.java
+++ CNT4007c/NetworkingSP14/src/NormalMessageTypes/Bitfield.java
@@ -1,13 +1,58 @@
 package NormalMessageTypes;
 
-import MessageTypes.NormalMessage;
-
-public class Bitfield extends NormalMessage{
-	public Byte[] bytes;
+public class Bitfield {
+	int length;
+	int[] payload;
 	
-	public Bitfield(String data, int senderID, Byte[] bytes)
+	public Bitfield(int length, int[] payload, boolean host)
 	{
-		super(data, senderID);
-		this.bytes = bytes;
+		this.length = length;
+		this.payload = payload;
+		
+		setBits(host);
+	}
+	
+	private void setBits(boolean host)
+	{
+		int indices = (length/8);
+		
+		if(length%8 != 0)
+		{
+			indices++;
+		}
+		
+		int[] payload = new int[indices];
+		
+		if(host)
+		{
+			for(int a = 0; a < indices; a++)
+			{
+				payload[a] = 1;
+			}
+		}
+		else
+		{
+			for(int a = 0; a < indices; a++)
+			{
+				payload[a] = 0;
+			}
+		}
+		
+		this.payload = payload;
+	}
+	public boolean checkCompletion()
+	{
+		if(!(length > 0))
+		{
+			return false;			
+		}
+		
+		for(int a = 0; a < length; a++)
+		{
+			if(payload[a] == 0)
+				return false;
+		}
+		
+		return true;
 	}
 }
